1801200bcdbd2fc0a15817e9a3b998f506473ed9
David Radjai <dradjai@yahoo.com>
2023-03-29 23:03:48 -0400

    homework convert min to secs and get sum of even numbers from array-ch

--- weekTwoDayThree.js
+++ weekTwoDayThree.js
@@ -15,7 +15,7 @@ const evenSum = arr => {
   let sum = 0;
   for(let i = 0; i < arr.length; i++){
      if (arr[i] % 2 === 0) {
-        sum = sum + arr[i];
+        sum += arr[i];
      }
     }
     return sum; 
